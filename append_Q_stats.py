454e11d9abaeaa768eb4e8f5d890825848621494
Kuang-Chung Wang <kuang-chungwang@Kuang-Chungs-MacBook-Pro.local>
2020-06-06 16:27:43 -0700

    getting rid of pickle

--- append_Q_stats.py
+++ append_Q_stats.py
@@ -3,15 +3,13 @@ import datetime as dt
 import os
 import pandas as pd
 import pandas_datareader.data as web
-import pickle
 import requests
 import pbratio4 
 
 
 def analyse_data_from_yahoo():
-    with open("sp500tickers.pickle", "rb") as f:
-        tickers = pickle.load(f)
-    label_list=['drop_ratio','price_on_0409','pb','roe','market_cap','Debt_Eq','Dividend_','Corre_spy_last200','Corre_spy_last100']
+    ticker_csv = pd.read_csv('sp500tickers.csv', parse_dates=True, index_col=0)
+    label_list=['drop_ratio','price_on_0409','pb','roe','market_cap','Debt_Eq','Dividend_','profit_margin','Corre_spy_last200','Corre_spy_last100']
     analysis = pd.DataFrame(columns=label_list)
     correlation = pd.DataFrame(columns=['spy', 'other'])
     spy = pd.read_csv('stock_dfs/{}.csv'.format('spy'), parse_dates=True, index_col=0)
@@ -20,10 +18,8 @@ def analyse_data_from_yahoo():
     #print(correlation)
     #analysis = pd.DataFrame(columns=['tick', 'ratio'])
     i=0
-    for ticker in tickers:
+    for ticker in ticker_csv.iloc[:,0]:
         i=i+1
-        #print(df.shape)
-        ticker = ticker[:-1]
         if os.path.exists('stock_dfs/{}.csv'.format(ticker)):
             df = pd.read_csv('stock_dfs/{}.csv'.format(ticker), parse_dates=True, index_col=0)
             correlation['other']=df['Adj Close']
@@ -32,10 +28,10 @@ def analyse_data_from_yahoo():
             #analysis.loc[i] =  [ticker  ,  df['10ma'].iloc[320]/df['10ma'].iloc[1] ]
             #analysis[ticker]  = [  df['10ma'].iloc[320]/df['10ma'].iloc[1] ]
             if df.shape[0]>328:
-                analysis.loc[ticker,label_list[0]]  =   df.loc['2020-04-29','10ma']/df.loc['2020-02-14','10ma'] 
+                analysis.loc[ticker,label_list[0]]  =   df.loc['2020-06-05','10ma']/df.loc['2020-02-14','10ma'] 
                 analysis.loc[ticker,label_list[1]]  =   df.loc['2020-04-29','Adj Close'] 
-                analysis.loc[ticker,label_list[7]]  = correlation.tail(200).corr().loc['spy','other'] 
-                analysis.loc[ticker,label_list[8]]  = correlation.tail(100).corr().loc['spy','other'] 
+                analysis.loc[ticker,label_list[8]]  = correlation.tail(200).corr().loc['spy','other'] 
+                analysis.loc[ticker,label_list[9]]  = correlation.tail(100).corr().loc['spy','other'] 
             print(ticker,i)
             try:
                 temp=pbratio4.get_price2book(ticker)    
@@ -45,6 +41,7 @@ def analyse_data_from_yahoo():
                 analysis.loc[ticker,label_list[4]]  =  temp[2] 
                 analysis.loc[ticker,label_list[5]]  =  temp[3] 
                 analysis.loc[ticker,label_list[6]]  =  temp[4] 
+                analysis.loc[ticker,label_list[7]]  =  temp[5] 
             except:
                 print("An exception occurred")
         else:
--- get_500list.py
+++ get_500list.py
@@ -3,7 +3,6 @@ import datetime as dt
 import os
 import pandas as pd
 import pandas_datareader.data as web
-import pickle
 import requests
 
 
@@ -27,11 +26,11 @@ def save_sp500_tickers():
     for row in table.findAll('tr')[1:]:
         ticker = row.findAll('td')[0].text
         print(ticker)
-        tickers.append(ticker)
-    tickers.append("spy ")
-    with open("sp500tickers.pickle", "wb") as f:
-        pickle.dump(tickers, f)
-    return tickers
+        tickers.append(ticker.rstrip())
+    tickers.append("spy")
+    tick_df = pd.DataFrame(tickers) 
+    tick_df.to_csv('sp500tickers.csv')
+    return "done"
 
 
 def get_data_from_yahoo(reload_sp500=False):
@@ -42,15 +41,15 @@ def get_data_from_yahoo(reload_sp500=False):
         #print(tickers)
 	
     else:
-        with open("sp500tickers.pickle", "rb") as f:
-            tickers = pickle.load(f)
+        ticker_csv = pd.read_csv('sp500tickers.csv', parse_dates=True, index_col=0)
+    
     if not os.path.exists('stock_dfs'):
         os.makedirs('stock_dfs')
     start = dt.datetime(2019, 1, 1)
     end = dt.date.today() #datetime(2020, 4, 18)
-    for ticker in tickers:
+    for ticker in ticker_csv.iloc[:,0]:
         # just in case your connection breaks, we'd like to save our progress!
-        ticker = ticker[:-1]
+        #ticker = ticker[:-1]
         if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
             print(ticker+" downloading") 
             df = web.DataReader(ticker, 'yahoo', start, end)
@@ -72,7 +71,7 @@ def get_data_from_yahoo(reload_sp500=False):
 #            analysis.loc[i] =  [ticker  ,  df['10ma'].iloc[320]/df['10ma'].iloc[1] ]
 #            print(analysis)
 
-#save_sp500_tickers()
+save_sp500_tickers()
 get_data_from_yahoo()
 
 
