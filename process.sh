b13ac7bdbe093b996ad0fce2397fd6ef04139d47
Kuang-Chung Wang <kuang-chungwang@KuangChungsMBP.localdomain>
2020-11-20 17:30:11 -0800

    update the main script

--- process.sh
+++ process.sh
@@ -1,10 +1,18 @@
+#install by the following 
+# python3  -m pip install yahoofinancials
+
 file=500_analysis.csv
+#rm stock_dfs/*
 
-
-sed  -i '' 's/[][]//g'  $file
-sed  -i '' "s/B'//g"  $file
-sed  -i '' 's/%//g'  $file
-sed  -i '' "s/'//g"  $file
-sed  -i '' "s/,,/,NA,/g"  $file
-sed  -i '' "s/,-,/,0,/g"  $file
-sed  -i '' "s/,- /,0,/g"  $file
+python3 get_500list.py   ## download historic data 
+python3 append_Q_stats.py  ##calculate  
+#
+##sed  -i '' 's/[][]//g'  $file
+#sed  -i '' "s/B,/,/g"  $file
+#sed  -i '' 's/%//g'  $file
+#sed  -i '' "s/'//g"  $file
+#sed  -i '' "s/,,/,NA,/g"  $file
+#sed  -i '' "s/,-,/,0,/g"  $file
+#sed  -i '' "s/,- /,0,/g"  $file
+#
+#python3 brain.py
