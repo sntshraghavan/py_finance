e2033bb58c41df9cee70058a830ec67db79cfe73
kuang <kevin60105@gmail.com>
2021-01-10 10:28:36 -0800

    micro fixes

--- process.sh
+++ process.sh
@@ -1,11 +1,14 @@
 #install by the following 
 # python3  -m pip install yahoofinancials
-
 file=500_analysis.csv
+cur_dir=$PWD
+echo $cur_dir 
+mkdir build
+cd build 
 #rm stock_dfs/*
 
-python3 get_500list.py   ## download historic data 
-python3 append_Q_stats.py  ##calculate  
+python3 $cur_dir/py_finance/get_500list.py   ## download historic data 
+python3 $cur_dir/py_finance/append_Q_stats.py  ##calculate  
 #
 ##sed  -i '' 's/[][]//g'  $file
 #sed  -i '' "s/B,/,/g"  $file
